d9f2888273221a5988132ad52d6b97bf51be952b
gjf2a <ferrer@hendrix.edu>
2024-08-25 04:25:21 -0500

    Removed earlier attempts with mean rests and median rests.

--- examples/long_rest_print.rs
+++ examples/long_rest_print.rs
@@ -1,8 +1,6 @@
 use midi_note_recorder::Recording;
 use music_analyzer_generator::{durations_notes_from, partitioned_melody};
 
-const MIN_LONG: f64 = 0.3;
-
 fn main() -> anyhow::Result<()> {
     let args = std::env::args().collect::<Vec<_>>();
     if args.len() < 2 {
@@ -11,33 +9,9 @@ fn main() -> anyhow::Result<()> {
     let recording: Recording = Recording::from_file(args[1].as_str())?;
     let durations_notes = durations_notes_from(&recording);
 
-    println!("Median rest time: {}", median_rest_time(&durations_notes));
-    println!("Mean rest time:   {}", mean_rest_time(&durations_notes));
-
-    for (d, n, v) in durations_notes.iter() {
-        println!("{d:.2}\t{n}\t{v}");
-        if *v == 0 && *d > MIN_LONG {
-            println!();
-        }
-    }
-
-    println!();
-    println!();
-    println!();
-
     let c = consolidated_times(&durations_notes);
     println!("num consolidated: {}", c.len());
 
-    for (d, n, v) in c.iter() {
-        println!("{d:.2}\t{n}\t{v}");
-        if *v == 0 && *d > MIN_LONG {
-            println!();
-        }
-    }
-
-    println!();
-    println!();
-    println!("Partitioned");
     let p = partitioned_melody(&c, 3);
     for interval in p.iter() {
         for i in interval.iter() {
@@ -47,25 +21,6 @@ fn main() -> anyhow::Result<()> {
     }
 
     Ok(())
-}
-
-fn rest_times(durations_notes: &Vec<(f64, u8, u8)>) -> Vec<f64> {
-    durations_notes
-        .iter()
-        .filter(|(_, _, v)| *v == 0)
-        .map(|(t, _, _)| *t)
-        .collect::<Vec<_>>()
-}
-
-fn median_rest_time(durations_notes: &Vec<(f64, u8, u8)>) -> f64 {
-    let mut rest_times = rest_times(durations_notes);
-    rest_times.sort_by(|f1, f2| f1.partial_cmp(f2).unwrap());
-    rest_times[rest_times.len() / 2]
-}
-
-fn mean_rest_time(durations_notes: &Vec<(f64, u8, u8)>) -> f64 {
-    let rt = rest_times(durations_notes);
-    rt.iter().sum::<f64>() / rt.len() as f64
 }
 
 fn consolidated_times(durations_notes: &Vec<(f64, u8, u8)>) -> Vec<(f64, u8, u8)> {
